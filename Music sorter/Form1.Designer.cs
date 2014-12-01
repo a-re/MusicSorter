7dc3c8aad771069f6285f04b1531c3768cecba54
Alex Restifo <calix1999@gmail.com>
2014-12-01 15:10:23 -0500

    First?

---
+++ Music sorter/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Music_sorter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInput2 = new System.Windows.Forms.TextBox();
+            this.txtInput1 = new System.Windows.Forms.TextBox();
+            this.btnBrowse2 = new System.Windows.Forms.Button();
+            this.btnBrowse1 = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.txtDebug = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.txtProgress = new System.Windows.Forms.ToolStripStatusLabel();
+            this.progress = new System.Windows.Forms.ToolStripProgressBar();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input directory:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Output directory:";
+            // 
+            // txtInput2
+            // 
+            this.txtInput2.BackColor = System.Drawing.Color.White;
+            this.txtInput2.Location = new System.Drawing.Point(98, 28);
+            this.txtInput2.Name = "txtInput2";
+            this.txtInput2.ReadOnly = true;
+            this.txtInput2.Size = new System.Drawing.Size(272, 20);
+            this.txtInput2.TabIndex = 2;
+            this.txtInput2.TextChanged += new System.EventHandler(this.txtInput2_TextChanged);
+            // 
+            // txtInput1
+            // 
+            this.txtInput1.BackColor = System.Drawing.Color.White;
+            this.txtInput1.Location = new System.Drawing.Point(98, 6);
+            this.txtInput1.Name = "txtInput1";
+            this.txtInput1.ReadOnly = true;
+            this.txtInput1.Size = new System.Drawing.Size(272, 20);
+            this.txtInput1.TabIndex = 3;
+            this.txtInput1.TextChanged += new System.EventHandler(this.txtInput1_TextChanged);
+            // 
+            // btnBrowse2
+            // 
+            this.btnBrowse2.Location = new System.Drawing.Point(373, 26);
+            this.btnBrowse2.Name = "btnBrowse2";
+            this.btnBrowse2.Size = new System.Drawing.Size(40, 23);
+            this.btnBrowse2.TabIndex = 4;
+            this.btnBrowse2.Text = ".....";
+            this.btnBrowse2.UseVisualStyleBackColor = true;
+            this.btnBrowse2.Click += new System.EventHandler(this.btnBrowse2_Click);
+            // 
+            // btnBrowse1
+            // 
+            this.btnBrowse1.Location = new System.Drawing.Point(373, 4);
+            this.btnBrowse1.Name = "btnBrowse1";
+            this.btnBrowse1.Size = new System.Drawing.Size(40, 23);
+            this.btnBrowse1.TabIndex = 5;
+            this.btnBrowse1.Text = ".....";
+            this.btnBrowse1.UseVisualStyleBackColor = true;
+            this.btnBrowse1.Click += new System.EventHandler(this.btnBrowse1_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Enabled = false;
+            this.btnSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSort.Location = new System.Drawing.Point(6, 49);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(407, 63);
+            this.btnSort.TabIndex = 6;
+            this.btnSort.Text = "Start";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // txtDebug
+            // 
+            this.txtDebug.BackColor = System.Drawing.Color.Black;
+            this.txtDebug.ForeColor = System.Drawing.Color.Lime;
+            this.txtDebug.Location = new System.Drawing.Point(6, 118);
+            this.txtDebug.Name = "txtDebug";
+            this.txtDebug.ReadOnly = true;
+            this.txtDebug.Size = new System.Drawing.Size(407, 157);
+            this.txtDebug.TabIndex = 7;
+            this.txtDebug.Text = "";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.txtProgress,
+            this.progress});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 278);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(415, 24);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // txtProgress
+            // 
+            this.txtProgress.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.txtProgress.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner;
+            this.txtProgress.Name = "txtProgress";
+            this.txtProgress.Size = new System.Drawing.Size(43, 19);
+            this.txtProgress.Text = "Ready";
+            // 
+            // progress
+            // 
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(100, 18);
+            this.progress.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 302);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtDebug);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnBrowse1);
+            this.Controls.Add(this.btnBrowse2);
+            this.Controls.Add(this.txtInput1);
+            this.Controls.Add(this.txtInput2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Music sorter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInput2;
+        private System.Windows.Forms.TextBox txtInput1;
+        private System.Windows.Forms.Button btnBrowse2;
+        private System.Windows.Forms.Button btnBrowse1;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.RichTextBox txtDebug;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel txtProgress;
+        private System.Windows.Forms.ToolStripProgressBar progress;
+    }
+}
+
---
+++ Music sorter/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Reflection;
+using System.IO;
+
+namespace Music_sorter
+{
+    public partial class Form1 : Form
+    {
+        bool t1 = false;
+        bool t2 = false;
+
+        string inputDir;
+        string outputDir;
+        string nl = Environment.NewLine;
+        List<string> musicFiles = new List<string>();
+        List<string> musicFilesNew = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtInput1_TextChanged(object sender, EventArgs e)
+        {
+            if (txtInput1.Text.Equals("")) { t1 = false; }
+            if (!txtInput1.Text.Equals("")) { t1 = true; }
+            TxtCheck();
+        }
+
+        private void txtInput2_TextChanged(object sender, EventArgs e)
+        {
+            if (txtInput2.Text.Equals("")) { t2 = false; }
+            if (!txtInput2.Text.Equals("")) { t2 = true; }
+            TxtCheck();
+        }
+
+        private void btnBrowse1_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fd = new FolderBrowserDialog())
+            {
+                fd.Description = "Select folder with your organized music";
+                if (fd.ShowDialog() == DialogResult.OK)
+                {
+                    txtInput1.Text = fd.SelectedPath;
+                    inputDir = fd.SelectedPath;
+                }
+            }
+        }
+
+        private void btnBrowse2_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fd = new FolderBrowserDialog())
+            {
+                fd.Description = "Select folder where you want your music to go";
+                if (fd.ShowDialog() == DialogResult.OK)
+                {
+                    txtInput2.Text = fd.SelectedPath;
+                    outputDir = fd.SelectedPath;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AddText("Music sorter for Google Play!" + nl);
+            AddText("by Alex Restifo" + nl);
+            AddText("----------------------------------" + nl + nl);
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            progress.Visible = true;
+            btnSort.Enabled = false;
+            txtProgress.Text = "Working...";
+            AddText("Starting..." + nl);
+
+            Thread workThread = new Thread(new ThreadStart(DoSort));
+            workThread.Priority = ThreadPriority.Highest;
+            workThread.Start();
+        }
+
+        public void DoSort()
+        {
+            DirectoryInfo parentDir = new DirectoryInfo(inputDir);
+            foreach (FileInfo f in parentDir.GetFiles())
+            {
+                musicFiles.Add(f.FullName);
+            }
+            DoFindMusic(parentDir);
+
+            UpdateProp(txtDebug, "Text", musicFiles.Count + " songs detected!" + nl + nl);
+            UpdateProp(txtDebug, "Text", "----------------------------" + nl + nl);
+
+            UpdatePropStatus(progress, "Maximum", musicFiles.Count);
+            DoCopyFiles();
+
+            if (MessageBox.Show("Would you like to take away track numbers?" + nl + "(01. <songname> --> <songname>)", "Info", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) { DoRemoveNumbers(); }
+
+            MessageBox.Show("Done copying " + musicFiles.Count + " files!");
+            UpdateProp(btnSort, "Enabled", true);
+            UpdatePropStatus(progress, "Visible", false);
+            UpdatePropStatus(txtProgress, "Text", "Ready");
+        }
+
+        private void DoRemoveNumbers()
+        {
+            UpdatePropStatus(txtProgress, "Text", "Renaming...");
+            for (int i = 0; i < musicFilesNew.Count; i++)
+            {
+                string oldFile = musicFilesNew[i];
+                if (!(oldFile.ToLower().EndsWith(".mp3") || oldFile.ToLower().EndsWith(".wav"))) { continue; }
+
+                string withoutExt = oldFile.Substring(0, oldFile.LastIndexOf("\\") + 1);
+                string rawFileName = oldFile.Substring(oldFile.LastIndexOf("\\") + 1);
+                string newFile = rawFileName.Substring(rawFileName.IndexOf(".") + 2);
+                string final = withoutExt + newFile;
+                File.Move(oldFile, final);
+            }
+        }
+
+        private void DoFindMusic(DirectoryInfo dir)
+        {
+            foreach (DirectoryInfo subDir in dir.GetDirectories("*", SearchOption.AllDirectories))
+            {
+                foreach (FileInfo f in subDir.GetFiles())
+                {
+                    if (f.Extension.ToLower() == ".mp3" || f.Extension.ToLower() == ".wav") // search for WAVs, I guess....
+                    {
+                        musicFiles.Add(f.FullName);
+                    }
+                }
+                if (subDir.GetDirectories().Length != 0)
+                {
+                    foreach (DirectoryInfo evenMoreSubDir in subDir.GetDirectories("*", SearchOption.AllDirectories))
+                    {
+                        DoFindMusic(evenMoreSubDir);
+                    }
+                }
+                else { continue; }
+            }
+        }
+
+        public void DoCopyFiles()
+        {
+            int i = 1;
+            foreach (string s in musicFiles)
+            {
+                string name = s.Substring(s.LastIndexOf("\\") + 1);
+                musicFilesNew.Add(outputDir + "\\" + name); //This is adding to a list which contains the locations of the NEW music files AFTER they are copied
+                File.Copy(s, outputDir + "\\" + name, true);
+
+                UpdateProp(txtDebug, "Text", "Copied file " + i + "!" + nl);
+                UpdatePropStatus(txtProgress, "Text", "Copied file " + i + "/" + musicFiles.Count);
+                UpdatePropStatus(progress, "Value", i);
+                i++;
+            }
+        }
+
+        private void TxtCheck()
+        {
+            if (t1 && t2 == true)
+            { btnSort.Enabled = true; }
+            else
+            { btnSort.Enabled = false; }
+        }
+
+        private void AddText(string txt)
+        {
+            txtDebug.Text += txt;
+        }
+
+        #region Thread-Safety
+        // UpdateProp() and UpdatePropStatus() allows for cross-threaded updates to the GUI, but it's ugly code.
+        private delegate void TSDelegate(Control control, string propertyName, object propertyValue);
+        private void UpdateProp(Control control, string propertyName, object propertyValue)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(new TSDelegate(UpdateProp), new object[] { control, propertyName, propertyValue });
+            }
+            else
+            {
+                control.GetType().InvokeMember(propertyName, BindingFlags.SetProperty, null, control, new object[] { propertyValue });
+            }
+        }
+
+        private delegate void ToolStripThreadSafeDelegate(ToolStripItem toolstripItem, string propertyName, object propertyValue);
+        private void UpdatePropStatus(ToolStripItem toolstripItem, string propertyName, object propertyValue)
+        {
+            if (toolstripItem.Owner.InvokeRequired)
+            {
+                ToolStripThreadSafeDelegate callback = new ToolStripThreadSafeDelegate(UpdatePropStatus);
+                toolstripItem.Owner.Invoke(callback, new object[] { toolstripItem, propertyName, propertyValue });
+            }
+            else
+            {
+                Type t = toolstripItem.GetType();
+                PropertyInfo p = t.GetProperty(propertyName);
+                p.SetValue(toolstripItem, propertyValue, null);
+            }
+        }
+        #endregion
+    }
+}
